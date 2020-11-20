bea75edd691de5a949538a8094fdc8fe753acd5a
Ariel Alvarado <>
2020-11-20 16:00:41 -0400

    Added styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="heart-box">
+      <div className="text">
+        <h1>Feliz cumpleaños mi vida!</h1>
+        <p>Espero que los cumplas muy pero muy feliz.</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Otro añito más que pasas a mi lado, me haces muy feliz, y espero, que
+          yo haga lo mismo por ti.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Sigue adelante, cambia siempre para mejor y nunca te rindas.</p>
+        <p>
+          Recuerda que nosotros siempre te estaremos apoyando en todo lo
+          posible, tu monito y tu ovejita.
+        </p>
+        <p>
+          <strong>Monito: </strong> Gracias por todo lo que hiciste por
+          nosotros, te quiero mucho. Un abrazo.
+        </p>
+        <div className="dibu">&nbsp;</div>
+        <h2>Táctica y estrategia</h2>
+        <p>
+          <strong>Mario Benedetti</strong>
+        </p>
+        <p>Mi táctica es</p>
+        <p>mirarte</p>
+        <p>aprender como sos</p>
+        <p>quererte como sos</p>
+        <p>mi táctica es</p>
+        <p>hablarte</p>
+        <p>y escucharte</p>
+        <p>construir con palabras</p>
+        <p>un puente indestructible</p>
+        <p>mi táctica es</p>
+        <p>quedarme en tu recuerdo</p>
+        <p>no sé cómo</p>
+        <p>ni sé</p>
+        <p>con qué pretexto</p>
+        <p>pero quedarme en vos</p>
+        <p>mi táctica es</p>
+        <p>ser franco</p>
+        <p>y saber que sos franca</p>
+        <p>y que no nos vendamos</p>
+        <p>simulacros</p>
+        <p>para que entre los dos</p>
+        <p>no haya telón</p>
+        <p>ni abismos</p>
+        <p>mi estrategia es</p>
+        <p>en cambio</p>
+        <p>más profunda y más simple</p>
+        <p>mi estrategia es</p>
+        <p>que un día cualquiera</p>
+        <p>no sé cómo</p>
+        <p>ni sé</p>
+        <p>con qué pretexto</p>
+        <p>por fin</p>
+        <p>me necesites.</p>
+        <p className="loveyou">Te amo conejita, Feliz Cumpleaños.</p>
+      </div>
+      <div className="heart-logo"></div>
+      <div className="heart animate-topright"></div>
+      <div className="heart animate-bottomright"></div>
+      <div className="heart animate-topleft"></div>
+      <div className="heart animate-bottomleft"></div>
+      <div className="heart animate-up"></div>
+      <div className="heart animate-down"></div>
+      <div className="heart animate-left"></div>
+      <div className="heart animate-right"></div>
     </div>
   );
 }
 
 export default App;
+
+<h1>Find Your Perfect Home</h1>;
